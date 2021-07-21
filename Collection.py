76da777c9c3a5844f111f2b3f5a33289aacbc938
Patrick O'Keefe <67643795+P-O-K@users.noreply.github.com>
2021-07-21 15:58:49 +0100

    Update Collection.py

--- Collection.py
+++ Collection.py
@@ -87,4 +87,4 @@ if __name__ == '__main__':
 	CLT = CollectionTree( )
 	print( CLT.count( 'the quick brown fox jumps over the lazy dog' ) )
 	CLT.empty( )
-	print( CLT.count( 'the quick brown fox jumps over the lazy dog', words=True ) )
+	print( '\n', CLT.count( 'the quick brown fox jumps over the lazy dog', words=True ) )
